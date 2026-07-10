24fe49b2c34679eac1f76513e65d9335cc31a18f
T Vicky <t.vicky@rosstechnologies.in>
2026-07-11 00:28:42 +0530

    feat: basic setup

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import express from "express"
-import 'dotenv/config'
+import cors from "cors"
 
 export const app = express()
+
+app.use(cors())
+app.use(express.urlencoded())
+app.use(express.static("public"))
 
 app.get("/", (_, res) => {
     res.send("Hello World")
